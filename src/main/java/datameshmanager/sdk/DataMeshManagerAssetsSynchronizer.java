63031f8601c465c56bc6ccec07e79625bc652e5b
Simon Harrer <simon.harrer@gmail.com>
2024-11-18 21:07:05 +0100

    UPDATE

--- src/main/java/datameshmanager/sdk/DataMeshManagerAssetsSynchronizer.java
+++ src/main/java/datameshmanager/sdk/DataMeshManagerAssetsSynchronizer.java
@@ -3,6 +3,7 @@ package datameshmanager.sdk;
 import datameshmanager.sdk.client.ApiException;
 import datameshmanager.sdk.client.model.Asset;
 import jakarta.annotation.PreDestroy;
+import java.time.Duration;
 import java.util.Objects;
 import java.util.Spliterator;
 import java.util.Spliterators;
@@ -23,13 +24,15 @@ public class DataMeshManagerAssetsSynchronizer {
   private final DataMeshManagerClient client;
   private final DataMeshManagerAgentRegistration agentRegistration;
   private final DataMeshManagerAssetsProvider assetsProvider;
-  private final ExecutorService executorService;
+  private volatile boolean stopped = false;
 
-  public DataMeshManagerAssetsSynchronizer(String id, DataMeshManagerClient client,
-      DataMeshManagerAssetsProvider assetsProvider, ExecutorService executorService) {
+  private Duration delay = Duration.parse("PT5S");
+
+  public DataMeshManagerAssetsSynchronizer(String id,
+      DataMeshManagerClient client,
+      DataMeshManagerAssetsProvider assetsProvider) {
     this.client = client;
     this.assetsProvider = assetsProvider;
-    this.executorService = executorService;
     this.agentRegistration = new DataMeshManagerAgentRegistration(client, id, "assets-asynchronizer");
 
     this.agentRegistration.register();
@@ -37,12 +40,33 @@ public class DataMeshManagerAssetsSynchronizer {
 
   public void start() {
     this.agentRegistration.up();
-    // TODO loop with delay
-    synchronizeDatabricksAssets();
+
+    while(!this.stopped) {
+      synchronizeDatabricksAssets();
+      delay(delay);
+    }
+  }
+
+  private void delay(Duration duration) {
+    long start = System.currentTimeMillis();
+    long end = start + duration.toMillis();
+    while(System.currentTimeMillis() < end) {
+      try {
+        Thread.sleep(end - System.currentTimeMillis());
+      } catch (InterruptedException ignored) {
+
+      }
+    }
+
   }
 
   public void stop() {
     this.agentRegistration.stop();
+    if (this.stopped) {
+      log.info("Already stopped asset synchronization");
+      return;
+    }
+    this.stopped = true;
     log.info("Stopped asset synchronization");
   }
 
